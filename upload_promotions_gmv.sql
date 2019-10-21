281fe41273f4b1427c36358bcc366617b661f7f9
Santi Bermejo <santiago.bermejo@mercadolibre.com>
2019-10-21 11:49:39 -0300

    Update upload_promotions_gmv.sql

--- upload_promotions_gmv.sql
+++ upload_promotions_gmv.sql
@@ -3,7 +3,8 @@ DROP TABLE BI.ATTRIB_EVENTS;
 --CREO TABLA TEMPORAL
 CREATE TABLE BI.ATTRIB_EVENTS
 (
-SIT_SITE_ID  STRING,
+    SIT_SITE_ID  STRING,
+    USER_ID STRING,
     UID  STRING,
     ITE_ITEM_ID  STRING,
     PLATFORM  STRING,
@@ -15,6 +16,7 @@ LOCATION 's3://melidata-results-batch/promotions/attrib_events/';
 INSERT INTO TABLE BI.ATTRIB_EVENTS
     SELECT
     distinct application.site_id as sit_site_id,
+    usr.user_id as user_id,
     usr.uid as uid,
     IF(get_json_object(event_data, '$.item_id') IS NULL,
         IF(get_json_object(event_data, '$.items') IS NULL,
@@ -26,14 +28,12 @@ INSERT INTO TABLE BI.ATTRIB_EVENTS
     CASE WHEN get_json_object(platform.fragment, '$.c_id') = '/home/promotions/element' THEN 0 ELSE 1 END as landing
     FROM tracks
     WHERE  path = '/vip'
-    and ds >=  cast(DATE_SUB(CURRENT_DATE,2) as string) 
-    and ds <   cast(CURRENT_DATE as string) 
-    and usr.uid is not null
-    and (get_json_object(platform.fragment, '$.deal_print_id') is not null or get_json_object(platform.fragment, '$.c_id') = '/home/promotions/element');
+      and ds >=  cast(DATE_SUB(CURRENT_DATE,2) as string) 
+      and ds <   cast(CURRENT_DATE as string) 
+      and usr.uid is not null
+      and (get_json_object(platform.fragment, '$.deal_print_id') is not null or get_json_object(platform.fragment, '$.c_id') = '/home/promotions/element');
 
 ALTER TABLE bi.promotions_gmv SET TBLPROPERTIES('EXTERNAL'='FALSE');
-
--- ALTER TABLE bi.promotions_gmv DROP IF EXISTS PARTITION (FECHA = cast(DATE_SUB(CURRENT_DATE,1) as string));
 
 -- GMV Atribuido por origen
 INSERT INTO TABLE bi.promotions_gmv PARTITION (fecha)
@@ -47,7 +47,7 @@ SELECT
       bids.CAT_CATEG_NAME_L1 AS category_name_L1,
       bids.CAT_CATEG_NAME_L2 AS category_name_L2,
       bids.CAT_CATEG_NAME_L3 AS category_name_L3,
-      at.platform,
+      orders.platform,
       at.landing,
       '24 HOURS' as atr_type,
       sum(bids.bid_base_current_price * bids.bid_quantity_ok) as GMVE,
@@ -58,25 +58,28 @@ INNER JOIN
     (SELECT
        application.site_id as sit_site_id,
        cast(get_json_object(event_data, '$.order_id') AS DECIMAL(18,0)) as ord_order_id,
+       usr.user_id as user_id,
        usr.uid as uid,
+       device.platform as platform,
        CONCAT(SUBSTR(server_timestamp,1,10),' ',SUBSTR(server_timestamp,12,8)) as order_exact_date,
        from_unixtime(unix_timestamp(CONCAT(SUBSTR(server_timestamp,0,10),' ',SUBSTR(server_timestamp,12,8)), 'yyyy-MM-dd HH:mm:ss') - 86400) as order_exact_date_sub_24h
     FROM default.tracks
     WHERE ds >=  concat(cast(DATE_SUB(CURRENT_DATE,1) as string), ' 00')
-    and ds <= concat(CURRENT_DATE, ' 00') --genera el mismo dia del odr_created_dt
-    and path = '/orders/ordercreated' ) orders
+      and ds <= concat(CURRENT_DATE, ' 00') --genera el mismo dia del odr_created_dt
+      and path = '/orders/ordercreated' ) orders
   on bids.ord_order_id = orders.ord_order_id
 INNER JOIN bi.attrib_events at
   ON at.sit_site_id = orders.sit_site_id
-  AND at.uid = orders.uid
+  AND (case when at.user_id IS NOT NULL then at.user_id = orders.user_id
+    else at.uid = orders.uid end)
   AND at.event_exact_date >= orders.order_exact_date_sub_24h
   AND at.event_exact_date <  orders.order_exact_date
   AND at.ite_item_id = trim(concat(bids.sit_site_id,cast(bids.ite_item_id as string)))
 WHERE bids.ite_gmv_flag = 1
-AND bids.mkt_marketplace_id = 'TM'
-AND bids.ord_created_dt  = DATE_SUB(CURRENT_DATE,1) 
-and bids.tim_day_winning_date is not null
-AND bids.ite_today_promotion_flag = 1
+  AND bids.mkt_marketplace_id = 'TM'
+  AND bids.ord_created_dt  = DATE_SUB(CURRENT_DATE,1) 
+  and bids.tim_day_winning_date is not null
+  AND bids.ite_today_promotion_flag = 1
 GROUP BY CONCAT(cast(bids.TIM_DAY_WINNING_DATE as string),' ',SUBSTR(CAST(bids.tim_time_winning_date+1000000 AS string),2,2)),
       SUBSTR(CAST(bids.tim_time_winning_date+1000000 AS string),2,2),
       bids.sit_site_id,
@@ -86,7 +89,7 @@ GROUP BY CONCAT(cast(bids.TIM_DAY_WINNING_DATE as string),' ',SUBSTR(CAST(bids.t
       bids.CAT_CATEG_NAME_L1,
       bids.CAT_CATEG_NAME_L2,
       bids.CAT_CATEG_NAME_L3,
-      at.platform,
+      orders.platform,
       at.landing,
       '24 HOURS',
       bids.TIM_DAY_WINNING_DATE;
@@ -105,7 +108,7 @@ SELECT
       bids.CAT_CATEG_NAME_L1 AS category_name_L1,
       bids.CAT_CATEG_NAME_L2 AS category_name_L2,
       bids.CAT_CATEG_NAME_L3 AS category_name_L3,
-      at.platform,
+      orders.platform,
       at.landing,
       '24 HOURS' as atr_type,
       sum(bids.bid_base_current_price * bids.bid_quantity_ok) as GMVE,
@@ -117,12 +120,13 @@ INNER JOIN
        application.site_id as sit_site_id,
        cast(get_json_object(event_data, '$.order_id') AS DECIMAL(18,0)) as ord_order_id,
        usr.uid as uid,
+       device.platform as platform,
        CONCAT(SUBSTR(server_timestamp,1,10),' ',SUBSTR(server_timestamp,12,8)) as order_exact_date,
        from_unixtime(unix_timestamp(CONCAT(SUBSTR(server_timestamp,1,10),' ',SUBSTR(server_timestamp,12,8)), 'yyyy-MM-dd HH:mm:ss') - 86400) as order_exact_date_sub_24h
     FROM default.tracks
     WHERE ds >=  concat(cast(DATE_SUB(CURRENT_DATE,1) as string), ' 00')
-    and ds <= concat(CURRENT_DATE, ' 00') --genera el mismo dia del odr_created_dt
-    and path = '/orders/ordercreated' ) orders
+      and ds <= concat(CURRENT_DATE, ' 00') --genera el mismo dia del odr_created_dt
+      and path = '/orders/ordercreated' ) orders
   on bids.ord_order_id = orders.ord_order_id
 INNER JOIN
        (select distinct sit_site_id,
@@ -135,15 +139,16 @@ INNER JOIN
         ) at
         ON
            at.sit_site_id = orders.sit_site_id
-          AND at.uid = orders.uid
+          AND (case when at.user_id IS NOT NULL then at.user_id = orders.user_id
+            else at.uid = orders.uid end)
           AND at.event_exact_date >= order_exact_date_sub_24h
           AND at.event_exact_date <  order_exact_date
           AND at.ite_item_id = trim(concat(bids.sit_site_id,cast(bids.ite_item_id as string)))
 WHERE bids.ite_gmv_flag = 1
-AND bids.mkt_marketplace_id = 'TM'
-AND bids.ord_created_dt  = DATE_SUB(CURRENT_DATE,1) 
-and bids.tim_day_winning_date is not null
-AND bids.ite_today_promotion_flag = 1
+  AND bids.mkt_marketplace_id = 'TM'
+  AND bids.ord_created_dt  = DATE_SUB(CURRENT_DATE,1) 
+  and bids.tim_day_winning_date is not null
+  AND bids.ite_today_promotion_flag = 1
 GROUP BY CONCAT(cast(bids.TIM_DAY_WINNING_DATE as string),' ',SUBSTR(CAST(bids.tim_time_winning_date+1000000 AS string),2,2)),
       SUBSTR(CAST(bids.tim_time_winning_date+1000000 AS string),2,2),
       bids.sit_site_id,
@@ -153,7 +158,7 @@ GROUP BY CONCAT(cast(bids.TIM_DAY_WINNING_DATE as string),' ',SUBSTR(CAST(bids.t
       bids.CAT_CATEG_NAME_L1,
       bids.CAT_CATEG_NAME_L2,
       bids.CAT_CATEG_NAME_L3,
-      at.platform,
+      orders.platform,
       at.landing,
       '24 HOURS',
       bids.TIM_DAY_WINNING_DATE;
@@ -202,7 +207,6 @@ GROUP BY CONCAT(cast(a11.TIM_DAY_WINNING_DATE as string),' ',SUBSTR(CAST(a11.tim
       a11.TIM_DAY_WINNING_DATE;
 
 -- GMV TOTAL SITE
--- GMV TOTAL SITE
 INSERT INTO TABLE bi.promotions_gmv PARTITION (fecha)
 SELECT
   CONCAT(cast(a11.TIM_DAY_WINNING_DATE as string),' ',SUBSTR(CAST(a11.tim_time_winning_date+1000000 AS string),2,2)) AS FechaHora,
@@ -237,11 +241,6 @@ GROUP BY CONCAT(cast(a11.TIM_DAY_WINNING_DATE as string),' ',SUBSTR(CAST(a11.tim
       100,
       'TOTAL SITE',
       a11.TIM_DAY_WINNING_DATE;
-
-ALTER TABLE bi.promotions_gmv SET TBLPROPERTIES('EXTERNAL'='TRUE');
-
---BORRO TABLE TEMPORAL
-DROP TABLE BI.ATTRIB_EVENTS;
 
 ALTER TABLE bi.promotions_gmv SET TBLPROPERTIES('EXTERNAL'='TRUE');
 
